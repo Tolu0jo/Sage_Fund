72f13053e0fb8ea4d67c9c684a79e6baecdb50b9
toluOjo <ojotolulope137@gmail.com>
2024-01-28 13:44:42 +0100

    implement transfer and withdraw fund

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { createAccount, fundAccount, getAccount } from "../controller/accountController";
+import { createAccount, fundAccount, getAccount, transferFund, withdrawFund } from "../controller/accountController";
 import { auth } from "../middleware/auth";
 
 const router = Router();
 
 router.post("/account",auth,createAccount);
 
-router.get("/account",auth,getAccount)
+router.get("/account",auth,getAccount);
 
-router.post("/account/fund",auth,fundAccount)
+router.post("/account/fund",auth,fundAccount);
 
+router.post("/account/transfer",auth,transferFund);
+
+router.post("/account/withdraw",auth,withdrawFund);
 export default router;
